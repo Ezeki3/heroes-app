8cabc7134e7f61516c4ee7d72a3b3735dba53e91
Ezequiel Lino <sonisombra@gmail.com>
2023-09-19 13:08:10 -0600

    autocomplete usando formControl

--- src/app/heroes/pages/search-page/search-page.component.ts
+++ src/app/heroes/pages/search-page/search-page.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl } from '@angular/forms';
 
 @Component({
   selector: 'app-search-page',
@@ -6,6 +7,8 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./search-page.component.css']
 })
 export class SearchPageComponent implements OnInit {
+
+  searchInput = new FormControl('');
 
   constructor() { }
 
