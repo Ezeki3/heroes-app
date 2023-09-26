12d363a4197be1236150f69079da9b790397d607
Ezequiel Lino <sonisombra@gmail.com>
2023-09-26 10:57:23 -0600

    guards en angular

---
+++ src/app/auth/guards/auth.guard.ts
@@ -0,0 +1,23 @@
+import { Injectable } from '@angular/core';
+import { ActivatedRouteSnapshot, CanActivate, CanMatch, Route, RouterStateSnapshot, UrlSegment, UrlTree } from '@angular/router';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthGuard implements CanMatch, CanActivate {
+
+  constructor() {}
+
+  canMatch(route: Route, segments: UrlSegment[]): boolean | Observable<boolean> {
+    console.log('can match');
+    console.log({route, segments});
+    return false;
+  }
+  canActivate(route: ActivatedRouteSnapshot, state: RouterStateSnapshot): boolean |  Observable<boolean>  {
+    console.log('can activate');
+    console.log({route, state});
+    return false;
+  }
+  
+}
